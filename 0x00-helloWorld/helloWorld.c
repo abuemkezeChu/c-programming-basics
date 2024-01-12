201156ba33002f9f1cd9c6ff6714a25deae4e6f9
Abuemkeze Chu <abuemkeze@gmail.com>
2024-01-12 19:24:42 +0100

    use escape sequence "\n" for newline character

--- 0x00-helloWorld/helloWorld.c
+++ 0x00-helloWorld/helloWorld.c
@@ -6,5 +6,7 @@
 int main()
 {
   printf("Hello World\n");
+  //logging a second sentence to the console
+  printf("My name is Mueghe Abuemkeze CHU ! an aspiring ethical hacker and systems engineer\n");
   return 0;
 }
