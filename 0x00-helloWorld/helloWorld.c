c7dc33739eb50deb34bef1c29470acbd1b5f26c4
Abuemkeze Chu <abuemkeze@gmail.com>
2024-01-13 10:20:36 +0100

    feat: make program dynamic
    use variables and format specifiers

--- 0x00-helloWorld/helloWorld.c
+++ 0x00-helloWorld/helloWorld.c
@@ -5,8 +5,12 @@
 
 int main()
 {
+  char name[] = "Mueghe Abuemkeze CHU";
+  int year = 2024;
+  int age = year - 2003;
+
   printf("Hello World\n");
-  //logging a second sentence to the console
-  printf("My name is Mueghe Abuemkeze CHU ! an aspiring ethical hacker and systems engineer\n");
+  // logging a second sentence to the console
+  printf("My name is %s ! I am %d years old, an aspiring ethical hacker and systems engineer\n", name,age);
   return 0;
 }
