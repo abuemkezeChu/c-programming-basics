b4e2600733e8c5af6bfd7094b236ca61136e2263
Abuemkeze Chu <abuemkeze@gmail.com>
2024-02-15 10:37:07 +0100

    implement logic

--- 0x01-basic_algorithm/sum_two.c
+++ 0x01-basic_algorithm/sum_two.c
@@ -1,5 +1,20 @@
 #include <stdio.h>
 int main()
 {
+  int x, y;
+  printf("Enter num1: ");
+  scanf("%d", &x);
+  printf("Enter num2: ");
+  scanf("%d", &y);
+  // check condition
+  if (x == y)
+  {
+    printf("For num1 == num2, Sum of the two numbers : %d\n", (x + y) * 3);
+  }
+  else
+  {
+    printf("Sum of the two numbers : %d\n", (x + y));
+  }
+
   return 0;
 }
