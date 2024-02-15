2e6c26f9b383433f72d96c6dbb08b153cf0764a5
Abuemkeze Chu <abuemkeze@gmail.com>
2024-02-15 09:43:47 +0100

    absolute difference calc

--- 0x01-basic_algorithm/abs_difference.c
+++ 0x01-basic_algorithm/abs_difference.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
+#include <math.h>
 int main()
 {
+  const int NUMBER = 51;
+  int n, diff;
+  printf("Enter a real number: ");
+  scanf("%d", &n);
+  // calculate the difference
+  diff = n - NUMBER;
+  printf("Absolute difference is : ", abs(diff));
   return 0;
 }
